def818ebc4009ee2c816f862f171001382a8a3ba
Eiren <109363685+EirenTheCoder@users.noreply.github.com>
2024-06-19 00:53:02 -0500

    Create boletaPago.java

---
+++ src/modelo/boletaPago.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author EirenGG
+ */
+public class boletaPago {
+    int numBol,Dni,id_prod,cant_prod;
+    public boletaPago(int numBol,int Dni,int id_prod,int cant_prod){
+        numBol = this.numBol;
+        Dni = this.Dni;
+        id_prod = this.id_prod;
+        cant_prod = this.cant_prod;
+        
+    }   
+    public int getNumBol(){
+    
+        return numBol;
+        
+    }
+    public int getDNI(){
+    
+        return Dni;
+       
+    }
+    
+     public int getId_Prod(){
+    
+        return id_prod;
+       
+    }
+     public int getCant_Prod(){
+    
+        return cant_prod;
+       
+    }
+     public void setNumBol(int numero_boleta){
+     
+         numBol = numero_boleta;
+     }
+    
+        public void setDNI(int nuevo_dni){
+     
+         Dni = nuevo_dni;
+     }
+        public void setID_Producto(int id_produc){
+     
+         
+            id_prod = id_produc;
+     }
+        public void setCantidad_Producto(int cant_produc){
+     
+         cant_prod = cant_produc;
+     }
+       
+    
+}
